9221b953583868be7b473196549740e2fc798fbe
duvergerR <remyduverger95@gmail.com>
2016-05-10 15:23:31 +0200

    rendu final

--- index_m.php
+++ index_m.php
@@ -3,8 +3,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <link rel="stylesheet" type="text/css" href="feuillecss.css" />
+<link rel="stylesheet" type="text/css" href="feuillecss.css" />
   <script type="text/javascript" src="f_javascript.js"></script>
 <title>Accueil</title>
 </head>
@@ -12,12 +11,18 @@
 <body>
 <div id="centrale">
     
-    <div id="entete"><img src="image/banniere_amis.jpg" width="600" height="300"></div>
-    <div id="menu">
-     <table align="center">
-            <tr>Bienvenue 
+    <div id="entete"><img src="image/easste.png" ></div>
+
+    <div id="main">     
+    
+    <table align="center">
+            <tr><h1>Bienvenue</h1>
             </tr>
-            
+	    
+	    <tr><a href="exemple_carte.html" target="_blank">
+		Voir votre position actuelle</a>
+	    </tr>            
+
             <tr>
                 <td><ul>Voir les événéments disponibles (lien a venir)</ul></td>    
             </tr>
@@ -35,15 +40,19 @@
         <form id="formDec" action ="deconnexion.php" method="POST">
 	<input type="submit" value="Déconnexion"/>
 	</form>
-    </div>
-    <div id="contenu">
-    </div>
-    <div id="pied">
-          <p> Contactez-nous : morganePielawski@etu.univ-st-etienne.fr /
-      remy.duverger@etu.univ-st-etienne.fr <br/>
-	&copy; Morgane Pielawski - Rémy Duverger </p>
+
+    <!-- fin div main -->
     </div>
 
+    <div id="pied">
+      <p> Contactez-nous : morgane.pielawski@etu.univ-st-etienne.fr /
+	remy.duverger@etu.univ-st-etienne.fr <br/>
+	&copy; Morgane Pielawski - Rémy Duverger <br/>
+	<a href="infos.php">Qui sommes-nous ?</a>
+      </p>
+    </div>
+
+<!-- fin div centrale-->
 </div>
 </body>
 </html>
